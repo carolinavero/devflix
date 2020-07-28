026b9995d7dc7c6c293765713c159a5d99bbd8b1
Carolina Vero <carolina@w42.com.br>
2020-07-28 12:22:21 -0300

    slider

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
+
 function CadastroCategoria() {
+
     return (
         <>
             <PageDefault>
-                <h1>Cadastro de Categoria</h1>
+                <h1>Nova Categoria</h1>
+
+                <form>
+                    <label htmlFor="">Nome: </label>
+                    <input type="text" />
+
+                    <label htmlFor="">Todos os vídeos: </label>
+                    <input type="text" />
+
+                    <label htmlFor="">Cor: </label>
+                    <input type="text" />
+
+                    <label htmlFor="">Código de segurança: </label>
+                    <input type="text" />
+
+                    <button>Cadastrar</button>
+
+                </form>
 
                 <Link to="/">
                     Ir para a Home
