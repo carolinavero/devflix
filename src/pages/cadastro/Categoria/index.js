efeaa94d5d9e430152ccf1bf2afd1b5f87e43229
Carolina Vero <carolina@w42.com.br>
2020-07-29 14:14:12 -0300

    funcao cadastro!

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
-
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+
+    const [categorias, setCategorias] = useState([]);
+    const initialValues = {
+        name: '',
+        description: '',
+        color: '',
+    };
+    const [values, setValues] = useState(initialValues);
+   
+    function setValue(chave, valor) {
+        setValues({
+            ...values,
+            [chave]: valor //valor dinamico - coloca []
+        })
+    }
+
+    function handleChange(infosDoEvento) {
+        //const { getAttribute, value } = e.target;
+        setValue(
+            infosDoEvento.target.getAttribute('name'), 
+            infosDoEvento.target.value
+        );
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('handle submit')
+        setCategorias([
+            ...categorias,
+            values
+        ]);
+
+        setValues(initialValues);
+    }
 
     return (
         <>
             <PageDefault>
-                <h1>Nova Categoria</h1>
+                <h1>Nova Categoria: {values.name} </h1>
 
-                <form>
-                    <label htmlFor="">Nome: </label>
-                    <input type="text" />
+                <form onSubmit={handleSubmit}>
+                   
+                    <FormField label="Nome da Categoria" name="name" type="text" value={values.name} onChange={handleChange} />
+                    <FormField label="Descrição" name="description" type="text" value={values.description} onChange={handleChange} />
+                    <FormField label="Cor" name="color" type="color" value={values.color} onChange={handleChange} />
 
+                    
                     <label htmlFor="">Todos os vídeos: </label>
-                    <input type="text" />
-
-                    <label htmlFor="">Cor: </label>
                     <input type="text" />
 
                     <label htmlFor="">Código de segurança: </label>
@@ -26,6 +60,14 @@ function CadastroCategoria() {
                     <button>Cadastrar</button>
 
                 </form>
+
+                <ul>
+                    {categorias.map((categoria, index) => {
+                        return (
+                            <li key={index}>{categoria.name}, {categoria.description}, {categoria.color}</li>
+                        )
+                    })}
+                </ul>
 
                 <Link to="/">
                     Ir para a Home
